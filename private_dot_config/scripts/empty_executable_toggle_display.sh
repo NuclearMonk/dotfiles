83ac4c2680c617a5f18afb30f1269a6a03040909
Manuel Soares <manuellisboasoares@gmail.com>
2025-03-13 15:01:07 +0200

    Update .config/scripts/toggle_display.sh

--- private_dot_config/scripts/empty_executable_toggle_display.sh
+++ private_dot_config/scripts/empty_executable_toggle_display.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 
-if  [[ $(brightnessctl -m g | grep -qz '^0$') -eq 0 ]]; then
+if  [[ $(brightnessctl -m g | grep -qz '^0$') -eq '0' ]]; then
 	echo WTF;
 	brightnessctl -r
 else
